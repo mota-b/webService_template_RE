459becd50ac46546fa4a70f8b9ea8fe5621c2e0d
moukhtar-b <gbenhatchi@hotmail.fr>
2018-01-03 02:18:18 +0000

    set the general navbar

--- React/src/components/Header/Header.jsx
+++ React/src/components/Header/Header.jsx
@@ -9,23 +9,41 @@ export class Header extends Component{
                 
                 <nav className="navbar navbar-inverse">
                     <div className="container-fluid">
-                        <div className="navbar-header">
-                            <a className="navbar-brand" href="#">WebSiteName</a>
+                        <div class="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a className="navbar-brand" href="#">React</a>
                         </div>
-                        <ul className="nav navbar-nav">
-                            <li className="active"><a href="#">Home</a></li>
-                            <li><a href="#">Page 1</a></li>
-                            <li><a href="#">Page 2</a></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                            <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
-                        </ul>
+            
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#/">Home <span className="sr-only">(current)</span></a></li>
+                                <li><a href="#">About</a></li>
+                                <li className="dropdown">
+                                
+                            </li>
+                            </ul>
+
+                            <ul className="nav navbar-nav navbar-inverse navbar-right">
+                                <li><a href="#/user">Guest</a></li>
+                                <li className="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Params<span class="caret"></span></a>
+                                    <ul className="dropdown-menu">
+                                        <li><a href="#/register"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                                        <li><a href="#/log"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="#/">Privacy Policy</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
-        
             </div>
-        
         );
     }
 };
